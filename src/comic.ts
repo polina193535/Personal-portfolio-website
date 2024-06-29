6266b5dad21043dfc9c16a5087197186b6880989
Polina <kostikova658@gmail.com>
2024-06-29 22:13:43 +0300

    done

--- src/comic.ts
+++ src/comic.ts
@@ -1,35 +1,41 @@
-import {formatDistanceToNow} from "date-fns";
+interface ComicInfo {
+  img: string;
+  alt: string;
+  safe_title: string;
+  year: number;
+  month: number;
+}
+
+import { formatDistanceToNow } from "date-fns";
 
 (async function () {
-            const id = await fetch(`https://fwd.innopolis.university/api/hw2?email=p.kostikova@innopolis.university`)
-                .then(response => response.json());
-            const info = await fetch(`https://fwd.innopolis.university/api/comic?id=${id}`)
-                .then(response => response.json());
-            console.log(info);
+  const id = await fetch(`https://fwd.innopolis.university/api/hw2?email=p.kostikova@innopolis.university`)
+    .then(response => response.json());
+  const info: ComicInfo = await fetch(`https://fwd.innopolis.university/api/comic?id=${id}`)
+    .then(response => response.json());
+  console.log(info);
 
-            const comic = document.createElement("article");
-            const img = document.createElement("img");
-            img.src = info.img;
-            img.alt = info.alt;
-            comic.appendChild(img);
+  const comic = document.createElement("article");
+  const img = document.createElement("img");
+  img.src = info.img;
+  img.alt = info.alt;
+  comic.appendChild(img);
 
-            const title = document.createElement("h2");
-            title.textContent = info.safe_title;
-            comic.appendChild(title);
+  const title = document.createElement("h2");
+  title.textContent = info.safe_title;
+  comic.appendChild(title);
 
-            const date = new Date(info.year, info.month - 1);
-            const time = document.createElement("time");
-            
-            const formattedDate = date.toLocaleDateString("en", { month:"long" ,year: "numeric" , });
-            time.appendChild( document .createTextNode(formattedDate));
-        
-            
-            const published = document.createElement("div");
-            published.textContent = "Published: ";
-            published.appendChild(time.cloneNode(true));
-            comic.appendChild(published);
-            
+  const date = new Date(info.year, info.month - 1);
+  const time = document.createElement("time");
 
-            const main = document.getElementsByTagName("main")[0];
-            main.appendChild(comic);
-        })();
+  const formattedDate = formatDistanceToNow(date, { addSuffix: true });
+  time.appendChild(document.createTextNode(formattedDate));
+
+  const published = document.createElement("div");
+  published.textContent = "Published: ";
+  published.appendChild(time.cloneNode(true));
+  comic.appendChild(published);
+
+  const main = document.getElementsByTagName("main")[0];
+  main.appendChild(comic);
+})();
